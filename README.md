7e9886166854cb4cc594ee2bed03b204e4e0ff9a
Chetan Satpute <chetan.satpute2002@gmail.com>
2023-10-12 02:08:49 +0530

    Setup Google Typescript Style

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Studious Doodle
+
+[![Code Style: Google](https://img.shields.io/badge/code%20style-google-blueviolet.svg)](https://github.com/google/gts)
--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    './node_modules/gts/',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -12,7 +13,8 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {allowConstantExport: true},
     ],
+    'node/no-unpublished-import': 'off',
   },
-}
+};
