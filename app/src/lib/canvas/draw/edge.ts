a522e5091f71feaad6b947883e0099b1b48ebeb0
Chetan Satpute <chetan.satpute2002@gmail.com>
2023-10-29 16:43:55 +0530

    Edge length can be changed based on percentage

--- app/src/lib/canvas/draw/edge.ts
+++ app/src/lib/canvas/draw/edge.ts
@@ -4,7 +4,7 @@ import {EdgeType, IEdge, IPoint} from '../types';
 export const HEAD_LENGTH = 10;
 
 export function drawEdge(ctx: CanvasRenderingContext2D, edge: IEdge) {
-  const {startNodePosition, endNodePosition, type} = edge;
+  const {startNodePosition, endNodePosition, type, percent} = edge;
 
   const startCenter = {
     x: startNodePosition.x + NODE_WIDTH / 2,
@@ -16,19 +16,16 @@ export function drawEdge(ctx: CanvasRenderingContext2D, edge: IEdge) {
     y: endNodePosition.y + NODE_HEIGHT / 2,
   };
 
-  const yDiff = endCenter.y - startCenter.y;
-  const xDiff = endCenter.x - startCenter.x;
-
-  let angle = Math.atan2(yDiff, xDiff);
-  if (angle < 0) {
-    // To prevent negative angle values
-    // convert angle in the range (0, 2 * Math.PI)
-    angle += Math.PI * 2;
-  }
+  const angle = getAngle(startCenter, endCenter);
 
   if (type === EdgeType.DIRECTED) {
     const startPoint = getEndPoint(startCenter, angle);
     const endPoint = getEndPoint(endCenter, (angle + Math.PI) % (Math.PI * 2));
+
+    const length = getLength(startPoint, endPoint);
+
+    endPoint.x = startPoint.x + (Math.cos(angle) * length * percent) / 100;
+    endPoint.y = startPoint.y + (Math.sin(angle) * length * percent) / 100;
 
     // line segment
     ctx.beginPath();
@@ -57,6 +54,11 @@ export function drawEdge(ctx: CanvasRenderingContext2D, edge: IEdge) {
     const startPoint = getEndPoint(startCenter, angle);
     const endPoint = getEndPoint(endCenter, (angle + Math.PI) % (Math.PI * 2));
 
+    const length = getLength(startPoint, endPoint);
+
+    endPoint.x = startPoint.x + (Math.cos(angle) * length * percent) / 100;
+    endPoint.y = startPoint.y + (Math.sin(angle) * length * percent) / 100;
+
     // line segment
     ctx.beginPath();
     ctx.moveTo(startPoint.x, startPoint.y);
@@ -65,6 +67,9 @@ export function drawEdge(ctx: CanvasRenderingContext2D, edge: IEdge) {
 
     ctx.stroke();
   } else if (type === EdgeType.DOUBLE_DIRECTED) {
+    const percentA = Math.floor(percent % 1000);
+    const percentB = Math.floor(percent / 1000);
+
     const startA = getEndPoint(startCenter, (angle + 0.1) % (Math.PI * 2));
     const startB = getEndPoint(
       startCenter,
@@ -80,55 +85,72 @@ export function drawEdge(ctx: CanvasRenderingContext2D, edge: IEdge) {
       (angle - 0.1 + Math.PI) % (Math.PI * 2)
     );
 
-    // line segment A
-    ctx.beginPath();
-    ctx.moveTo(startA.x, startA.y);
-    ctx.lineTo(endB.x, endB.y);
+    const lengthA = getLength(startA, endB);
+    const lengthB = getLength(startB, endA);
 
-    // line segment B
-    ctx.moveTo(startB.x, startB.y);
-    ctx.lineTo(endA.x, endA.y);
-    ctx.closePath();
+    endB.x = startA.x + (Math.cos(angle) * lengthA * percentA) / 100;
+    endB.y = startA.y + (Math.sin(angle) * lengthA * percentA) / 100;
 
-    ctx.stroke();
+    startB.x = endA.x - (Math.cos(angle) * lengthB * percentB) / 100;
+    startB.y = endA.y - (Math.sin(angle) * lengthB * percentB) / 100;
 
-    // head on segment A
-    ctx.beginPath();
-    ctx.moveTo(endB.x, endB.y);
-    ctx.lineTo(
-      endB.x - HEAD_LENGTH * Math.cos(angle - Math.PI / 7),
-      endB.y - HEAD_LENGTH * Math.sin(angle - Math.PI / 7)
-    );
+    if (getLength(startA, endB) >= HEAD_LENGTH) {
+      // line segment A
+      ctx.beginPath();
+      ctx.moveTo(startA.x, startA.y);
+      ctx.lineTo(endB.x, endB.y);
+      ctx.closePath();
 
-    ctx.lineTo(
-      endB.x - HEAD_LENGTH * Math.cos(angle),
-      endB.y - HEAD_LENGTH * Math.sin(angle)
-    );
+      ctx.stroke();
 
-    ctx.moveTo(endB.x, endB.y);
+      // head on segment A
+      ctx.beginPath();
+      ctx.moveTo(endB.x, endB.y);
+      ctx.lineTo(
+        endB.x - HEAD_LENGTH * Math.cos(angle - Math.PI / 7),
+        endB.y - HEAD_LENGTH * Math.sin(angle - Math.PI / 7)
+      );
 
-    ctx.closePath();
+      ctx.lineTo(
+        endB.x - HEAD_LENGTH * Math.cos(angle),
+        endB.y - HEAD_LENGTH * Math.sin(angle)
+      );
 
-    ctx.fill();
+      ctx.moveTo(endB.x, endB.y);
 
-    // head on segment B
-    ctx.beginPath();
-    ctx.moveTo(startB.x, startB.y);
-    ctx.lineTo(
-      startB.x + HEAD_LENGTH * Math.cos(angle - Math.PI / 7),
-      startB.y + HEAD_LENGTH * Math.sin(angle - Math.PI / 7)
-    );
+      ctx.closePath();
 
-    ctx.lineTo(
-      startB.x + HEAD_LENGTH * Math.cos(angle),
-      startB.y + HEAD_LENGTH * Math.sin(angle)
-    );
+      ctx.fill();
+    }
 
-    ctx.moveTo(startB.x, startB.y);
+    if (getLength(startB, endA) >= HEAD_LENGTH) {
+      // line segment B
+      ctx.beginPath();
+      ctx.moveTo(startB.x, startB.y);
+      ctx.lineTo(endA.x, endA.y);
+      ctx.closePath();
 
-    ctx.closePath();
+      ctx.stroke();
 
-    ctx.fill();
+      // head on segment B
+      ctx.beginPath();
+      ctx.moveTo(startB.x, startB.y);
+      ctx.lineTo(
+        startB.x + HEAD_LENGTH * Math.cos(angle - Math.PI / 7),
+        startB.y + HEAD_LENGTH * Math.sin(angle - Math.PI / 7)
+      );
+
+      ctx.lineTo(
+        startB.x + HEAD_LENGTH * Math.cos(angle),
+        startB.y + HEAD_LENGTH * Math.sin(angle)
+      );
+
+      ctx.moveTo(startB.x, startB.y);
+
+      ctx.closePath();
+
+      ctx.fill();
+    }
   }
 }
 
@@ -189,4 +211,25 @@ function getEndPoint(center: IPoint, angle: number) {
   }
 
   return point;
+}
+
+function getAngle(start: IPoint, end: IPoint): number {
+  const yDiff = end.y - start.y;
+  const xDiff = end.x - start.x;
+
+  let angle = Math.atan2(yDiff, xDiff);
+  if (angle < 0) {
+    // To prevent negative angle values
+    // convert angle in the range (0, 2 * Math.PI)
+    angle += Math.PI * 2;
+  }
+
+  return angle;
+}
+
+function getLength(start: IPoint, end: IPoint): number {
+  const yDiff = end.y - start.y;
+  const xDiff = end.x - start.x;
+
+  return Math.sqrt(yDiff * yDiff + xDiff * xDiff);
 }
--- server/src/lib/board/frame.ts
+++ server/src/lib/board/frame.ts
@@ -21,6 +21,7 @@ export interface IEdge {
   startNodePosition: IPoint;
   endNodePosition: IPoint;
   type: EdgeType;
+  percent: number;
 }
 
 export interface ILabel {
