05015ba458bade145fde2191022d708068c64efe
Chetan Satpute <chetan.satpute2002@gmail.com>
2023-11-27 10:53:09 +0530

    Removed manual https configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import fs from 'fs';
-import https from 'https';
-import path from 'path';
 
 import apiRouter from './routes/apiRouter';
 import {logger} from './middleware/logger';
 
-import {NODE_ENV, PORT} from './constant/env';
+import {PORT} from './constant/env';
 
 const app = express();
 
@@ -20,21 +17,25 @@ app.use('/api', apiRouter);
 app.get('/', (_req, res) => res.send({message: 'Studious Doodle API'}));
 app.get('/ping', (_req, res) => res.send({message: 'pong'}));
 
-if (NODE_ENV === 'production') {
-  const keyPath = path.join(__dirname, '..', 'certs', 'private-key.pem');
-  const certPath = path.join(__dirname, '..', 'certs', 'certificate.pem');
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT} 🚀`);
+});
 
-  const privateKey = fs.readFileSync(keyPath);
-  const certificate = fs.readFileSync(certPath);
-
-  const credentials = {key: privateKey, cert: certificate};
-  const httpsServer = https.createServer(credentials, app);
-
-  httpsServer.listen(PORT, () => {
-    console.log(`Server started on port ${PORT} 🚀`);
-  });
-} else {
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT} 🚀`);
-  });
-}
+// if (NODE_ENV === 'production') {
+//   const keyPath = path.join(__dirname, '..', 'certs', 'private-key.pem');
+//   const certPath = path.join(__dirname, '..', 'certs', 'certificate.pem');
+//
+//   const privateKey = fs.readFileSync(keyPath);
+//   const certificate = fs.readFileSync(certPath);
+//
+//   const credentials = {key: privateKey, cert: certificate};
+//   const httpsServer = https.createServer(credentials, app);
+//
+//   httpsServer.listen(PORT, () => {
+//     console.log(`Server started on port ${PORT} 🚀`);
+//   });
+// } else {
+//   app.listen(PORT, () => {
+//     console.log(`Server started on port ${PORT} 🚀`);
+//   });
+// }
